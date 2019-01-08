316582f4da2176105f3b0b714820023486189afb
liua0 <17713411393@163.com>
2019-01-08 11:22:01 +0800

    a

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var http = require('http'),
-    fileSystem = require('fs'),
-    path = require('path');
+// var http = require('http'),
+//     fileSystem = require('fs'),
+//     path = require('path');
 
-var server = http.createServer(function(request, response) {
-    var filePath = path.join(__dirname, 'semantic.min.css');
-    var stat = fileSystem.statSync(filePath);
-    response.writeHead(200, {
-        'Content-Type': 'text/css',
-        'Content-Length': stat.size,
-    });
-    var readStream = fileSystem.createReadStream(filePath);
-    readStream.pipe(response);
+// var server = http.createServer(function(request, response) {
+//     var filePath = path.join(__dirname, 'semantic.min.css');
+//     var stat = fileSystem.statSync(filePath);
+//     response.writeHead(200, {
+//         'Content-Type': 'text/css',
+//         'Content-Length': stat.size,
+//     });
+//     var readStream = fileSystem.createReadStream(filePath);
+//     readStream.pipe(response);
 
-});
+// });
 
 var port = process.env.PORT || 1337;
-server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+// console.log("Server running at http://localhost:%d", port);
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {};
+handle["/one"] = requestHandlers.one;
+handle["/two"] = requestHandlers.two;
+
+server.start(router.route, handle);
