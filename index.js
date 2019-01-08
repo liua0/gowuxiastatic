70fab5c4aa0c875b33e76db9ccf9fa1d7f2a4f93
liua0 <17713411393@163.com>
2019-01-08 16:08:23 +0800

    a

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var app = express();
 app.use("/",express.static(__dirname + "/public"));
-app.listen(port);
-
 var port = process.env.PORT || 1337;
 
 console.log("Server running at http://localhost:%d", port);
+app.listen(port);
+
+
