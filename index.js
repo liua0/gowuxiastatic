e45c9f1146491f1e1373bada41b553e4b13dd50c
liua0 <17713411393@163.com>
2019-01-08 15:58:21 +0800

    a

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var port = process.env.PORT || 1337;
 
 app.use("/",express.static(__dirname + "/public"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port)
